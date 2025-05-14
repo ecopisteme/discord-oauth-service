190d86477a638d79a5dfa5e728f443ee234c9829
ecopisteme <ecopisteme@gmail.com>
2025-05-14 17:43:31 +0800

    Create discord.js

---
+++ api/auth/discord.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: 'code',
+    scope: 'identify'
+  });
+  res.redirect(`https://discord.com/api/oauth2/authorize?${params}`);
+}
